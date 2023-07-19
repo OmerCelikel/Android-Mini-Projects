5e4b5127b3d14cac6ce2eeede438685332e6c1de
Ömer Oğuz Çelikel <omeroguz.celikel@tebarf.com.tr>
2023-07-19 11:30:57 +0300

    recylerView added

---
+++ RecyclerViewExample/app/src/main/java/com/celikel/recyclerviewexample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.celikel.recyclerviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView contactsRecView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactsRecView = findViewById(R.id.contactsRecView);
+    }
+}
