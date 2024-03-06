3b657131fda5ff4657bae05924c55aebcf648fdc
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-03-06 10:19:09 +0300

    extractStirngResource

--- AndroidDev/BirthdayCardApp/app/src/main/java/com/celikel/birthdaycardapp/MainActivity.kt
+++ AndroidDev/BirthdayCardApp/app/src/main/java/com/celikel/birthdaycardapp/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,7 +62,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
@@ -94,8 +95,8 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
 fun GreetingPreview() {
     BirthdayCardAppTheme {
         GreetingImage(
-            message = "Happy Birthday Sam!",
-            from = "From Emma"
+            message = stringResource(R.string.happy_birthday_sam),
+            from = stringResource(R.string.signature_text)
         )
     }
 }
